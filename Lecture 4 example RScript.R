589027dbee9c1c9f05c24a4276ff4a6f3dc337a1
kyleraychel <kyleraychel@gmail.com>
2021-04-08 15:44:37 -0700

    added function and lightYear dist

--- Lecture 4 example RScript.R
+++ Lecture 4 example RScript.R
@@ -6,3 +6,11 @@ lightYear <- lightDay*365
 cat("length of light year:", lightYear, "\n" )
 cat("That's too long")
 
+add <- function(x, y) {
+  x + y
+}
+
+lightYear <- function(ly) {
+  dist = ly * lightYear
+  return(dist)
+}
